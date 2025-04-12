16e479a7967f4b6c1ab92bbd184615b8cfa08755
Minh <S3929376@rmit.edu.vn>
2025-04-13 00:35:37 +0700

    frontend
    hello

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+
+import LoginRegister from './Components/LoginRegister/LoginRegister.jsx'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <LoginRegister/>
+    </div>
+  );
+}
+
+export default App
