2fd916ba525879d99db7011630addd329ea8f919
Nhat Minh Le Vo <103168635+NhatMinh0708@users.noreply.github.com>
2025-04-29 22:23:43 +0700

    Add files via upload

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,12 +5,14 @@ import './App.css';
 import LoginPage from './pages/LoginPage.jsx';
 import RegisterPage from './pages/RegisterPage.jsx';
 import ForgotPasswordPage from './pages/ForgotPasswordPage.jsx';
+
 import AdminDashboard from './pages/Admin/AdminDashBoard.jsx';
 import Settings from './pages/Admin/Settings.jsx';
-import { Toaster } from 'react-hot-toast';
+
+import AttendeeDashboard from './pages/User/AttendeeDashboard.jsx';
+import OrganizerDashboard from './pages/User/OrganizerDashboard.jsx'; // Fix tên sai: Orrganizer
 
 import { UserContextProvider } from './pages/User/UserContext';
-
 import axios from 'axios';
 
 axios.defaults.baseURL = 'http://localhost:3000';
@@ -20,11 +22,20 @@ function App() {
   return (
     <UserContextProvider>
       <Routes>
+        {/* Auth */}
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/forgotpassword" element={<ForgotPasswordPage />} />
-        <Route path="/admin" element={<AdminDashboard />} />
+
+        {/* Admin */}
+        <Route path="/admin/dashboard" element={<AdminDashboard />} />
         <Route path="/admin/settings" element={<Settings />} />
+
+        {/* Organizer */}
+        <Route path="/organizer/dashboard" element={<OrganizerDashboard />} />
+
+        {/* Attendee */}
+        <Route path="/attendee/dashboard" element={<AttendeeDashboard />} />
       </Routes>
     </UserContextProvider>
   );
